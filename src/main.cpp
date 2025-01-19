75cddf7e87e3c862cce8196b6c98aac560e26c91
Jim Chen <totally.not.jims.project@gmail.com>
2025-01-18 21:29:47 -0800

    Removed unnecessary comments

--- src/main.cpp
+++ src/main.cpp
@@ -16,94 +16,16 @@ bool piston_state = false;                              // true = extended
 int leftmove, rightmove;                                // Motor angular offset (?)
 bool toggle = false;                                    // Auxiliary variable for debouncing piston actuation
 
+
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
+ * Auxiliary functions
  */
-void on_center_button() {
-        /*
-        static bool pressed = false;
-        pressed = !pressed;
-        if (pressed) {
-                pros::lcd::set_text(2, "I was pressed!");
-        } else {
-                pros::lcd::clear_line(2);
-        }*/
+int curve_velocity(int velocity) {
+        /* Maps linear ouput to cubic curve */
+        int curve = (velocity * velocity * velocity) / (127 * 127);
+
+        return curve;
 }
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competitiocd::clear_line(2);
-        }n modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-
-void initialize() {
-        pros::lcd::initialize();
-        // pros::lcd::register_btn1_cb(on_center_button);
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-
-
-
-void autonomous() {
-        constexpr float ratio = 20.42035224;
-
-        leftmove = 127;
-        rightmove = -127;
-        left_mg.move(leftmove * ratio);
-        right_mg.move(rightmove * ratio);
-
-        pros::delay(AUTONOMOUS_STAGE_RUNTIME);
-        left_mg.move(0);
-        right_mg.move(0);
-}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 
 int abs(int x) {
         return (x < 0) ? -x : x;
@@ -141,12 +63,73 @@ void get_controller_input(int *leftmove, int *rightmove) {
         #endif
 }
 
-int curve_velocity(int velocity) {
-        /* Maps linear ouput to cubic curve */
-        int curve = (velocity * velocity * velocity) / (127 * 127);
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
 
-        return curve;
+void initialize() {
+        pros::lcd::initialize();
 }
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+
+void autonomous() {
+        constexpr float ratio = 20.42035224;
+
+        leftmove = 127;
+        rightmove = -127;
+        left_mg.move(leftmove * ratio);
+        right_mg.move(rightmove * ratio);
+
+        pros::delay(AUTONOMOUS_STAGE_RUNTIME);
+        left_mg.move(0);
+        right_mg.move(0);
+}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
 
 void opcontrol() {
         constexpr int LOOP_DELAY = 20;
